happy-corporate-fox/intellij-rust

commit 8044fb90a98402200446d11fb03d3ff08a99673b

PSI: rework macro call grammar

src/main/kotlin/org/rust/ide/inspections/RsTryMacroInspection.kt
@@ -9,10 +9,11 @@ import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
-import org.rust.lang.core.psi.RsTryExpr
-import org.rust.lang.core.psi.RsTryMacro
-import org.rust.lang.core.psi.RsVisitor
+import org.rust.lang.core.psi.RsMacroCall
 import org.rust.lang.core.psi.RsPsiFactory
+import org.rust.lang.core.psi.RsTryExpr
+import org.rust.lang.core.psi.RsVisitor
+import org.rust.lang.core.psi.ext.macroName
 
 /**
  * Change `try!` macro to `?` operator.
@@ -21,21 +22,24 @@ class RsTryMacroInspection : RsLocalInspectionTool() {
     override fun getDisplayName() = "try! macro usage"
 
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) = object : RsVisitor() {
-        override fun visitTryMacro(o: RsTryMacro) = holder.registerProblem(
-            o.macroInvocation,
-            "try! macro can be replaced with ? operator",
-            object : LocalQuickFix {
-                override fun getName() = "Change try! to ?"
+        override fun visitMacroCall(o: RsMacroCall) {
+            if (o.macroName?.text != "try" || o.tryMacroArgument == null) return
+            holder.registerProblem(
+                o,
+                "try! macro can be replaced with ? operator",
+                object : LocalQuickFix {
+                    override fun getName() = "Change try! to ?"
 
-                override fun getFamilyName() = name
+                    override fun getFamilyName() = name
 
-                override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
-                    val macro = descriptor.psiElement.parent as RsTryMacro
-                    val body = macro.tryMacroArgs?.expr ?: return
-                    val tryExpr = RsPsiFactory(project).createExpression("${body.text}?") as RsTryExpr
-                    macro.replace(tryExpr)
+                    override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+                        val macro = descriptor.psiElement as RsMacroCall
+                        val body = macro.tryMacroArgument!!.expr
+                        val tryExpr = RsPsiFactory(project).createExpression("${body.text}?") as RsTryExpr
+                        macro.replace(tryExpr)
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 }

src/main/grammars/RustParser.bnf
@@ -83,10 +83,6 @@
 
     extends("Pat(Wild|Ref|Tup|Vec|Macro|Struct|Enum|Ident|Range|Uniq|QualPath)") = Pat
 
-    elementType("ExprLikeMacro|ItemLikeMacro") = Macro
-    extends("(Try|FormatLike|Assert|AssertEq)Macro") = Macro
-    elementType(".+MacroInvocation|MacroRulesInvocation") = MacroInvocation
-
     generateTokenAccessors=true
 
     consumeTokenMethod("(.*_recover)|(.*Expr)|BinAssignOp") = "consumeTokenFast"
@@ -235,7 +231,7 @@ private Item ::= Constant
                | UseItem
                | ExternCrateItem
                | MacroDefinition
-               | MacroItem
+               | MacroCall
 
 private Item_with_recover ::= !('}' | <<eof>>) Item {
   pin = 1
@@ -665,7 +661,7 @@ private TypeTraitImpl ::= TypeReference WhereClause? TraitOrImplBody { pin = 1 }
 
 private TraitOrImplBody ::= '{' InnerAttr* Member* '}' { pin = 1 }
 
-private Member ::= !'}' ( Function | Constant | TypeAlias | ImplMacroMember ) {
+private Member ::= !'}' ( Function | Constant | TypeAlias | MacroCall ) {
   pin = 1
   recoverWhile = Member_recover
 }
@@ -729,11 +725,7 @@ TupleType ::= '(' [ <<comma_separated_list TypeReference>> ] ')' {
   stubClass = "org.rust.lang.core.stubs.RsPlaceholderStub"
   elementTypeFactory = "org.rust.lang.core.stubs.StubImplementationsKt.factory"
 }
-
-private TupleType_upper ::= (',' TypeReference)* ','? ')' {
-  stubClass = "org.rust.lang.core.stubs.RsTypeElementStub"
-  elementTypeFactory = "org.rust.lang.core.stubs.StubImplementationsKt.factory"
-}
+private TupleType_upper ::= (',' TypeReference)* ','? ')'
 
 Bound ::= Lifetime | TraitRef {
   extends = "org.rust.lang.core.psi.ext.RsStubbedElementImpl<?>"
@@ -989,7 +981,7 @@ LitExpr ::= STRING_LITERAL | BYTE_STRING_LITERAL
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-MacroDefinition ::= AttrsAndVis "macro_rules" '!' identifier MacroDefinitionBody {
+MacroDefinition ::= AttrsAndVis "macro_rules" '!' identifier MacroDefinitionBody <<macroSemicolon>>{
   implements = [ "org.rust.lang.core.psi.ext.RsNamedElement"
                  "org.rust.lang.core.psi.ext.RsOuterAttributeOwner" ]
   extends = "org.rust.lang.core.psi.ext.RsMacroDefinitionImplMixin"
@@ -998,13 +990,7 @@ MacroDefinition ::= AttrsAndVis "macro_rules" '!' identifier MacroDefinitionBody
   pin = 2
 }
 
-MacroDefinitionBody ::=
-  '(' MacroDefinitionCases ')' ';'
-| '[' MacroDefinitionCases ']' ';'
-| '{' MacroDefinitionCases '}'
-{ pin = 1 }
-
-private MacroDefinitionCases ::= (MacroDefinitionCase ';'?)*
+MacroDefinitionBody ::= <<any_braces (MacroDefinitionCase ';'?)*>>
 
 MacroDefinitionCase ::= MacroPattern '=>' MacroExpansion
 
@@ -1031,81 +1017,58 @@ MacroReference ::= '$' <<macroIdentifier>> {
 MacroExpansionReferenceGroup ::= '$' '(' MacroExpansionContents ')'
 
 
-fake Macro ::= MacroInvocation MacroArg?
 
-// Various kinds of macros
-ExprLikeMacro ::= MacroInvocation MacroArg
-ItemLikeMacro ::= MacroInvocation ItemMacroArg
-
-TryMacro ::= TryMacroInvocation TryMacroArgs { pin = 1 }
-FormatLikeMacro ::= FormatLikeMacroInvocation FormatMacroArgs { pin = 1 }
-AssertMacro ::= AssertMacroInvocation AssertMacroArgs { pin = 1 }
-AssertEqMacro ::= AssertEqMacroInvocation AssertEqMacroArgs { pin = 1 }
-
-VecMacro ::= VecMacroInvocation VecMacroArgs { pin = 1 }
-LogMacro ::= LogMacroInvocation LogMacroArgs { pin = 1 }
-
-private BuildInMacro ::= TryMacro
-                       | FormatLikeMacro
-                       | AssertMacro
-                       | AssertEqMacro
-                       | VecMacro
-                       | LogMacro
-
-private ZzMacroCall ::= BuildInMacro | ExprLikeMacro { pin(".*") = MacroInvocation }
-
-// Invocations
-MacroInvocation ::= identifier '!' identifier? {
+MacroCall ::= MacroHead <<macroSemicolon>> {
   implements = [ "org.rust.lang.core.psi.ext.RsReferenceElement" ]
-  mixin = "org.rust.lang.core.psi.ext.RsMacroInvocationImplMixin"
+  pin = 2
+  hooks = [ leftBinder = "DOC_COMMENT_BINDER" ]
+  mixin = "org.rust.lang.core.psi.ext.RsMacroCallImplMixin"
 }
 
-TryMacroInvocation ::= "try" '!'
-VecMacroInvocation ::= "vec" '!'
-LogMacroInvocation ::= ( "trace" | "log" | "warn" | "debug" | "error" | "info" ) '!'
+MacroCallNoSemicolons ::= MacroHead {
+  pin = 2
+  elementType = MacroCall
+  hooks = [ leftBinder = "DOC_COMMENT_BINDER" ]
+}
+
+private MacroHead ::= AttrsAndVis (SpecialMacro | identifier '!' identifier? MacroArgument)
+
+MacroArgument ::= <<any_braces TT>>
+TT ::= (<<any_braces TT>> | <<unpairedToken>>)*
+
+
+private SpecialMacro ::= TryMacro | FormatLikeMacro | AssertMacro | VecMacro | LogMacro
+
+private TryMacro ::= "try" '!' TryMacroArgument { pin = 2 }
+TryMacroArgument ::= <<any_braces AnyExpr>>
 
 // https://doc.rust-lang.org/std/fmt/
-FormatLikeMacroInvocation ::= ( "format"
-                              | "format_args"
-                              | "write"
-                              | "writeln"
-                              | "print"
-                              | "println"
-                              | "panic") '!'
+private FormatLikeMacro ::=
+  ("format" | "format_args" | "write" | "writeln" | "print" | "println" | "panic")
+  '!' FormatMacroArgument { pin = 2 }
 
-AssertMacroInvocation ::= ( "assert" | "debug_assert" ) '!'
-
-AssertEqMacroInvocation ::= ( "assert_eq"
-                            | "assert_ne"
-                            | "debug_assert_eq"
-                            | "debug_assert_ne") '!'
-
-// Arguments
-MacroArg ::= <<any_braces TokenTrees>>
-
-ItemMacroArg ::= '(' TokenTrees ')' ';'
-               | '{' TokenTrees '}'
-               | '[' TokenTrees ']' ';' {
-  pin = 1
-  elementType = MacroArg
-}
-
-TryMacroArgs ::= <<any_braces AnyExpr>>
-VecMacroArgs ::= '[' ArrayInitializer ']' | '(' ArrayInitializer ')' | '{' ArrayInitializer '}' {
-  pin = 1
-}
-
-private ZzLogMacroArgs ::= ('target' ':' Expr ','?)? (<<comma_separated_list FormatMacroArg>>)?
-LogMacroArgs ::=  <<any_braces ZzLogMacroArgs>>
-
-private ZzAssertMacroArgs ::= AnyExpr [ ',' <<comma_separated_list FormatMacroArg>> ]
-AssertMacroArgs ::= <<any_braces ZzAssertMacroArgs>>
-
-private ZzAssertEqMacroArgs ::= AnyExpr ',' AnyExpr [ ',' <<comma_separated_list FormatMacroArg>> ]
-AssertEqMacroArgs ::= <<any_braces ZzAssertEqMacroArgs>>
-
-FormatMacroArgs ::= <<any_braces [ <<comma_separated_list FormatMacroArg>> ] >>
+FormatMacroArgument ::= <<any_braces [ <<comma_separated_list FormatMacroArg>> ] >>
 FormatMacroArg ::= [ identifier '=' ] AnyExpr
+
+
+private AssertMacro ::=
+  ("assert" | "debug_assert" | "assert_eq" | "assert_ne" | "debug_assert_eq" | "debug_assert_ne")
+  '!' AssertMacroArgument { pin = 2 }
+
+AssertMacroArgument ::= <<any_braces (AnyExpr [ ',' <<comma_separated_list FormatMacroArg>> ])>>
+
+
+private VecMacro ::= "vec" '!' VecMacroArgument { pin = 2 }
+VecMacroArgument ::= <<any_braces ArrayInitializer >>
+
+
+private LogMacro ::=
+  ( "trace" | "log" | "warn" | "debug" | "error" | "info" )
+  '!' LogMacroArgument { pin = 2 }
+
+LogMacroArgument ::=
+  <<any_braces (('target' ':' Expr ','?)? (<<comma_separated_list FormatMacroArg>>)?)>>
+
 
 private meta any_braces ::=
   '(' <<param>> ')'
@@ -1114,29 +1077,8 @@ private meta any_braces ::=
 { pin = 1 }
 
 // Macro wrappers
-MacroExpr ::= ZzMacroCall
-PatMacro  ::= ZzMacroCall
-// TODO: Maybe implement item-related stuff for this production?
-MacroItem ::= AttrsAndVis ItemLikeMacro {
-  hooks = [ leftBinder = "DOC_COMMENT_BINDER" ]
-  implements = [ "org.rust.lang.core.psi.ext.RsOuterAttributeOwner"
-                 "org.rust.lang.core.psi.ext.RsNamedElement" ]
-  stubClass = "org.rust.lang.core.stubs.RsMacroItemStub"
-  elementTypeFactory = "org.rust.lang.core.stubs.StubImplementationsKt.factory"
-  mixin = "org.rust.lang.core.psi.ext.RsMacroItemImplMixin"
-}
-ImplMacroMember ::= ItemLikeMacro {
-  pin(".*") = MacroInvocation
-  hooks = [ leftBinder = "DOC_COMMENT_BINDER" ]
-}
-
-private TokenTrees ::= TokenTree*
-
-TokenTree ::= '(' TokenTrees ')'
-            | '{' TokenTrees '}'
-            | '[' TokenTrees ']'
-            | << unpairedToken >> +
-
+MacroExpr ::= MacroCallNoSemicolons
+PatMacro ::= MacroCallNoSemicolons
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Statements

src/main/kotlin/org/rust/ide/annotator/RsErrorAnnotator.kt
@@ -19,7 +19,6 @@ import com.intellij.psi.util.PsiTreeUtil
 import org.rust.cargo.project.workspace.PackageOrigin
 import org.rust.cargo.project.workspace.cargoWorkspace
 import org.rust.ide.annotator.fixes.*
-import org.rust.ide.utils.isNullOrEmpty
 import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.ext.*
 import org.rust.lang.core.resolve.Namespace
@@ -315,7 +314,7 @@ class RsErrorAnnotator : Annotator, HighlightRangeExtension {
             holder.createErrorAnnotation(traitRef, "The trait `$traitName` requires an `unsafe impl` declaration [E0200]")
         }
         // Macros can add methods
-        if (impl.implMacroMemberList.isNotEmpty()) return
+        if (impl.macroCallList.isNotEmpty()) return
 
         val implemented = impl.functionList.associateBy { it.name }
 

src/main/kotlin/org/rust/ide/annotator/RsHighlightingAnnotator.kt
@@ -21,7 +21,7 @@ class RsHighlightingAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
         val (partToHighlight, color) = when {
             element is RsPatBinding && !element.isReferenceToConstant -> highlightNotReference(element)
-            element is RsMacroInvocation -> highlightNotReference(element)
+            element is RsMacroCall -> highlightNotReference(element)
             element is RsReferenceElement -> highlightReference(element)
             else -> highlightNotReference(element)
         } ?: return
@@ -79,7 +79,7 @@ private fun colorFor(element: RsCompositeElement): RsColor? = when (element) {
     is RsMacroDefinition -> RsColor.MACRO
 
     is RsAttr -> RsColor.ATTRIBUTE
-    is RsMacroInvocation -> RsColor.MACRO
+    is RsMacroCall -> RsColor.MACRO
     is RsSelfParameter -> RsColor.SELF_PARAMETER
     is RsTryExpr -> RsColor.Q_OPERATOR
     is RsTraitRef -> RsColor.TRAIT
@@ -120,9 +120,14 @@ private fun partToHighlight(element: RsCompositeElement): TextRange? {
         return range
     }
 
-    val name =  when (element) {
+    if (element is RsMacroCall) {
+        var range = element.macroName?.textRange ?: return null
+        range = range.union(element.excl.textRange)
+        return range
+    }
+
+    val name = when (element) {
         is RsAttr -> element
-        is RsMacroInvocation -> element
         is RsSelfParameter -> element.self
         is RsTryExpr -> element.q
         is RsTraitRef -> element.path.identifier

src/main/kotlin/org/rust/ide/folding/RsFoldingBuilder.kt
@@ -80,7 +80,7 @@ class RsFoldingBuilder : FoldingBuilderEx(), DumbAware {
 
         override fun visitModItem(o: RsModItem) = foldBetween(o, o.lbrace, o.rbrace)
 
-        override fun visitMacroArg(o: RsMacroArg) = foldBetween(o, o.lbrace, o.rbrace)
+        override fun visitMacroArgument(o: RsMacroArgument) = foldBetween(o, o.lbrace, o.rbrace)
 
         override fun visitImplItem(o: RsImplItem) = foldBetween(o, o.lbrace, o.rbrace)
 

src/main/kotlin/org/rust/ide/formatter/RsFormattingModelBuilder.kt
@@ -16,8 +16,7 @@ import org.rust.ide.formatter.blocks.RsMacroArgFmtBlock
 import org.rust.ide.formatter.blocks.RsMultilineStringLiteralBlock
 import org.rust.lang.core.psi.RS_RAW_LITERALS
 import org.rust.lang.core.psi.RS_STRING_LITERALS
-import org.rust.lang.core.psi.RsElementTypes.MACRO_ARG
-import org.rust.lang.core.psi.RsElementTypes.MACRO_DEFINITION_BODY
+import org.rust.lang.core.psi.RsElementTypes.*
 
 class RsFormattingModelBuilder : FormattingModelBuilder {
     override fun getRangeAffectingIndent(file: PsiFile?, offset: Int, elementAtOffset: ASTNode?): TextRange? = null
@@ -39,7 +38,7 @@ class RsFormattingModelBuilder : FormattingModelBuilder {
         ): ASTBlock {
             val type = node.elementType
 
-            if (type == MACRO_DEFINITION_BODY || type == MACRO_ARG) {
+            if (type == MACRO_DEFINITION_BODY || type == MACRO_ARGUMENT) {
                 return RsMacroArgFmtBlock(node, alignment, indent, wrap, ctx)
             }
 

src/main/kotlin/org/rust/ide/formatter/impl/spacing.kt
@@ -24,7 +24,6 @@ import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.RsElementTypes.*
 import org.rust.lang.core.psi.ext.RsItemElement
 import org.rust.lang.core.psi.ext.RsNamedElement
-import org.rust.lang.core.psi.ext.elementType
 import org.rust.lang.core.psi.ext.getNextNonCommentSibling
 import org.rust.lang.core.psi.ext.getPrevNonCommentSibling
 import com.intellij.psi.tree.TokenSet.create as ts
@@ -131,10 +130,9 @@ fun createSpacingBuilder(commonSettings: CommonCodeStyleSettings, rustSettings: 
         .betweenInside(ELSE, BLOCK, ELSE_BRANCH).spacing(1, 1, 0, false, 0)
 
         //== macros
-        .betweenInside(IDENTIFIER, EXCL, MACRO_INVOCATION).spaces(0)
-        .betweenInside(IDENTIFIER, EXCL, MACRO_DEFINITION).spaces(0)
-        .betweenInside(MACRO_INVOCATION, IDENTIFIER, MACRO_DEFINITION).spaces(1)
-        .betweenInside(IDENTIFIER, MACRO_ARG, MACRO_DEFINITION).spaces(1)
+        .beforeInside(EXCL, MACRO_CALL).spaces(0)
+        .beforeInside(EXCL, MACRO_DEFINITION).spaces(0)
+        .afterInside(EXCL, MACRO_DEFINITION).spaces(1)
         .betweenInside(IDENTIFIER, MACRO_DEFINITION_BODY, MACRO_DEFINITION).spaces(1)
 
         //== rules with very large area of application
@@ -152,8 +150,8 @@ fun Block.computeSpacing(child1: Block?, child2: Block, ctx: RsFmtContext): Spac
             -> return createSpacing(1, 1, 0, true, 0)
 
         // Determine spacing between macro invocation and it's arguments
-            psi1 is RsMacroInvocation && psi2.elementType in MACRO_ARGS
-            -> return if (child2.node.firstChildNode.elementType == LBRACE) {
+            parentPsi is RsMacroCall && elementType1 == EXCL
+            -> return if (node2.chars.first() == '{' || elementType2 == IDENTIFIER) {
                 createSpacing(1, 1, 0, false, 0)
             } else {
                 createSpacing(0, 0, 0, false, 0)

src/main/kotlin/org/rust/ide/formatter/impl/utils.kt
@@ -20,7 +20,7 @@ import org.rust.lang.core.psi.ext.RsItemElement
 import org.rust.lang.core.psi.ext.RsMod
 import com.intellij.psi.tree.TokenSet.create as ts
 
-val SPECIAL_MACRO_ARGS = ts(FORMAT_MACRO_ARGS, LOG_MACRO_ARGS, TRY_MACRO_ARGS, VEC_MACRO_ARGS, ASSERT_EQ_MACRO_ARGS)
+val SPECIAL_MACRO_ARGS = ts(FORMAT_MACRO_ARGUMENT, LOG_MACRO_ARGUMENT, TRY_MACRO_ARGUMENT, VEC_MACRO_ARGUMENT, ASSERT_MACRO_ARGUMENT)
 
 val NO_SPACE_AROUND_OPS = ts(COLONCOLON, DOT, DOTDOT)
 val SPACE_AROUND_OPS = TokenSet.andNot(RS_OPERATORS, NO_SPACE_AROUND_OPS)
@@ -48,8 +48,6 @@ val DELIMITED_BLOCKS = orSet(BRACE_DELIMITED_BLOCKS, BRACK_DELIMITED_BLOCKS,
 val FLAT_BRACE_BLOCKS = orSet(MOD_ITEMS, ts(PAT_STRUCT, TRAIT_ITEM, IMPL_ITEM))
 
 val TYPES = ts(ARRAY_TYPE, REF_LIKE_TYPE, FN_POINTER_TYPE, TUPLE_TYPE, BASE_TYPE, FOR_IN_TYPE)
-
-val MACRO_ARGS = orSet(ts(MACRO_ARG), SPECIAL_MACRO_ARGS)
 
 val FN_DECLS = ts(FUNCTION, FN_POINTER_TYPE, LAMBDA_EXPR)
 

commit 24e85fb16de1406fe1347253e9125f63c99e2f1b

Fix exceptions after running the "Change try! to ?" inspection
A try!() invocation looks like this in PSI:
RsMacroExpr
-RsMacroCall
--identifier
--!
--RsTryMacroArgument

RsTryMacroInspection registers the problem and the fix on the RsMacroCall.
When the fix is applied, it replaces the RsMacroCall node with a RsTryExpr,
leaving back a corrupt RsMacroExpr missing a required RsMacroCall child.

Later some other code trips over this corrupt PSI node
triggering an exception in PsiElementBase.notNullChild().

This problem was introduced by commit 8044fb90a9
which reorganized macro related grammar.

Fix the problem by visiting and working on the RsMacroExpr instead.

src/main/kotlin/org/rust/ide/inspections/RsTryMacroInspection.kt
@@ -9,10 +9,7 @@ import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
-import org.rust.lang.core.psi.RsMacroCall
-import org.rust.lang.core.psi.RsPsiFactory
-import org.rust.lang.core.psi.RsTryExpr
-import org.rust.lang.core.psi.RsVisitor
+import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.ext.macroName
 
 /**
@@ -22,8 +19,8 @@ class RsTryMacroInspection : RsLocalInspectionTool() {
     override fun getDisplayName() = "try! macro usage"
 
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) = object : RsVisitor() {
-        override fun visitMacroCall(o: RsMacroCall) {
-            if (o.macroName?.text != "try" || o.tryMacroArgument == null) return
+        override fun visitMacroExpr(o: RsMacroExpr) {
+            if (o.macroCall.macroName?.text != "try" || o.macroCall.tryMacroArgument == null) return
             holder.registerProblem(
                 o,
                 "try! macro can be replaced with ? operator",
@@ -33,8 +30,8 @@ class RsTryMacroInspection : RsLocalInspectionTool() {
                     override fun getFamilyName() = name
 
                     override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
-                        val macro = descriptor.psiElement as RsMacroCall
-                        val body = macro.tryMacroArgument!!.expr
+                        val macro = descriptor.psiElement as RsMacroExpr
+                        val body = macro.macroCall.tryMacroArgument!!.expr
                         val tryExpr = RsPsiFactory(project).createExpression("${body.text}?") as RsTryExpr
                         macro.replace(tryExpr)
                     }
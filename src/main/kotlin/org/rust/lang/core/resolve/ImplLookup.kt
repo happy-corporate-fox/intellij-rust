happy-corporate-fox/intellij-rust

commit ce747debcccda3c801e0c29678403a7b4eb891f0

Cleanup

src/main/kotlin/org/rust/lang/core/resolve/ImplLookup.kt
@@ -300,11 +300,8 @@ class ImplLookup(
             is SelectionCandidate.Impl -> {
                 ctx.combineTraitRefs(ref, candidate.ref)
                 val candidateSubst = candidate.subst + mapOf(TyTypeParameter.self() to ref.selfTy)
-                val obligations = candidate.item.bounds.asSequence()
-                    .map { it.substitute(candidateSubst) }
-                    .map { ctx.normalizeAssociatedTypesIn(it, recursionDepth) }
-                    .flatMap { it.obligations.asSequence() + Obligation(newRecDepth, Predicate.Trait(it.value)) }
-                    .toList()
+                ctx.instantiateBounds(candidate.item.bounds, candidateSubst)
+                val obligations = ctx.instantiateBounds(candidate.item.bounds, candidateSubst).toList()
                 Selection(candidate.item, obligations, candidateSubst)
             }
             is SelectionCandidate.DerivedTrait -> Selection(candidate.item, emptyList())

src/main/kotlin/org/rust/lang/core/types/infer/TypeInference.kt
@@ -668,15 +668,8 @@ private class RsFnInferenceContext(
                 map
             }
         }
-        instantiateBounds(element.bounds, map)
+        ctx.instantiateBounds(element.bounds, map).forEach(fulfill::registerPredicateObligation)
         return map
-    }
-
-    private fun instantiateBounds(bounds: List<TraitRef>, subst: Map<TyTypeParameter, Ty>) {
-        bounds.asSequence()
-                .map { it.substitute(subst) }
-                .map(this::normalizeAssociatedTypesIn)
-                .forEach { fulfill.registerPredicateObligation(Obligation(Predicate.Trait(it))) }
     }
 
     private fun <T : TypeFoldable<T>> normalizeAssociatedTypesIn(ty: T): T {

commit c03dbeba738931509eff0e493b53f9fc522a51eb

Fixed recursion depth counting broken with ce747deb

src/main/kotlin/org/rust/lang/core/resolve/ImplLookup.kt
@@ -315,8 +315,7 @@ class ImplLookup(
             is SelectionCandidate.Impl -> {
                 ctx.combineTraitRefs(ref, candidate.ref)
                 val candidateSubst = candidate.subst + mapOf(TyTypeParameter.self() to ref.selfTy)
-                ctx.instantiateBounds(candidate.item.bounds, candidateSubst)
-                val obligations = ctx.instantiateBounds(candidate.item.bounds, candidateSubst).toList()
+                val obligations = ctx.instantiateBounds(candidate.item.bounds, candidateSubst, newRecDepth).toList()
                 Selection(candidate.item, obligations, candidateSubst)
             }
             is SelectionCandidate.DerivedTrait -> Selection(candidate.item, emptyList())
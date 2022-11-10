happy-corporate-fox/intellij-rust

commit 064f360895a510f1e381bebae9cd7142ead09d25

Fix issue #9468 random false-positive `Variable is never used`

src/main/kotlin/org/rust/lang/core/macros/MacroExpansionManager.kt
@@ -66,6 +66,7 @@ import org.rust.stdext.RsResult.Err
 import org.rust.stdext.RsResult.Ok
 import org.rust.taskQueue
 import java.io.IOException
+import java.lang.ref.SoftReference
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -936,10 +937,15 @@ private fun expandMacroOld(call: RsMacroCall): MacroExpansionCachedResult {
 }
 
 private fun expandMacroToMemoryFile(call: RsPossibleMacroCall, storeRangeMap: Boolean): MacroExpansionCachedResult {
+    val modificationTrackers = getModificationTrackersForMemExpansion(call)
+
+    val oldCachedResultSoftReference = call.getUserData(RS_EXPANSION_RESULT_SOFT)
+    extractExpansionResult(oldCachedResultSoftReference, modificationTrackers)?.let { return it }
+
     val def = call.resolveToMacroWithoutPsiWithErr()
-        .unwrapOrElse { return memExpansionResult(call, Err(it.toExpansionError())) }
+        .unwrapOrElse { return CachedValueProvider.Result(Err(it.toExpansionError()), modificationTrackers) }
     val crate = call.containingCrate
-    if (crate is FakeCrate) return memExpansionResult(call, Err(GetMacroExpansionError.Unresolved))
+    if (crate is FakeCrate) return CachedValueProvider.Result(Err(GetMacroExpansionError.Unresolved), modificationTrackers)
     val result = FunctionLikeMacroExpander.forCrate(crate).expandMacro(
         def,
         call,
@@ -969,7 +975,46 @@ private fun expandMacroToMemoryFile(call: RsPossibleMacroCall, storeRangeMap: Bo
         }
     }
 
-    return memExpansionResult(call, result)
+    if (result is Ok) {
+        val newCachedResult = CachedMemExpansionResult(result, modificationTrackers.modificationCount(), call)
+
+        // We want to guarantee that only one expansion RsFile per macro call exists in the memory at a time.
+        // That is, if someone holds a strong reference to expansion PSI, the SoftReference must not be cleared.
+        // This is possible if the expansion RsFile holds a strong reference to the object under SoftReference.
+        result.ok.file.putUserData(RS_EXPANSION_RESULT, newCachedResult)
+
+        val newCachedResultSoftReference = SoftReference(newCachedResult)
+        var prevReference = oldCachedResultSoftReference
+        while (!call.replace(RS_EXPANSION_RESULT_SOFT, prevReference, newCachedResultSoftReference)) {
+            prevReference = call.getUserData(RS_EXPANSION_RESULT_SOFT)
+            extractExpansionResult(prevReference, modificationTrackers)?.let { return it }
+        }
+    }
+
+    return CachedValueProvider.Result(result, modificationTrackers)
+}
+
+private val RS_EXPANSION_RESULT_SOFT: Key<SoftReference<CachedMemExpansionResult>> =
+    Key("org.rust.lang.core.macros.RS_EXPANSION_RESULT_SOFT")
+private val RS_EXPANSION_RESULT: Key<CachedMemExpansionResult> = Key("org.rust.lang.core.macros.RS_EXPANSION_RESULT")
+
+private data class CachedMemExpansionResult(
+    val result: RsResult<MacroExpansion, GetMacroExpansionError>,
+    val modificationCount: Long,
+    // Guarantees the macro call is retained in the memory if links to expansion PSI exist
+    val call: RsPossibleMacroCall,
+)
+
+private fun extractExpansionResult(
+    cachedResultSoftReference: SoftReference<CachedMemExpansionResult>?,
+    modificationTrackers: Array<ModificationTracker>,
+): CachedValueProvider.Result<RsResult<MacroExpansion, GetMacroExpansionError>>? {
+    val cachedResult = cachedResultSoftReference?.get()
+    if (cachedResult != null && cachedResult.modificationCount == modificationTrackers.modificationCount()) {
+        return CachedValueProvider.Result(cachedResult.result, modificationTrackers)
+    }
+
+    return null
 }
 
 private fun memExpansionResult(
@@ -977,12 +1022,22 @@ private fun memExpansionResult(
     result: RsResult<MacroExpansion, GetMacroExpansionError>
 ): MacroExpansionCachedResult {
     // Note: the cached result must be invalidated when `RsFile.cachedData` is invalidated
+    val modificationTrackers = getModificationTrackersForMemExpansion(call)
+    return CachedValueProvider.Result.create(result, modificationTrackers)
+}
+
+// Note: the cached result must be invalidated when `RsFile.cachedData` is invalidated
+private fun getModificationTrackersForMemExpansion(call: RsPossibleMacroCall): Array<ModificationTracker> {
+    val structureModTracker = call.rustStructureOrAnyPsiModificationTracker
     return if (call is RsMacroCall) {
-        CachedValueProvider.Result.create(result, call.rustStructureOrAnyPsiModificationTracker, call.modificationTracker)
+        arrayOf(structureModTracker, call.modificationTracker)
     } else {
-        CachedValueProvider.Result.create(result, call.rustStructureOrAnyPsiModificationTracker)
+        arrayOf(structureModTracker)
     }
 }
+
+private fun Array<ModificationTracker>.modificationCount(): Long =
+    sumOf { it.modificationCount }
 
 private val RS_EXPANSION_MACRO_CALL = Key.create<RsPossibleMacroCall>("org.rust.lang.core.psi.RS_EXPANSION_MACRO_CALL")
 

src/main/kotlin/org/rust/lang/core/psi/RsPsiManager.kt
@@ -270,7 +270,7 @@ val Crate.rustStructureModificationTracker: ModificationTracker
  * Returns [RsPsiManager.rustStructureModificationTracker] or [PsiModificationTracker.MODIFICATION_COUNT]
  * if `this` element is inside language injection
  */
-val RsElement.rustStructureOrAnyPsiModificationTracker: Any
+val RsElement.rustStructureOrAnyPsiModificationTracker: ModificationTracker
     get() {
         val containingFile = containingFile
         return when {
@@ -279,10 +279,10 @@ val RsElement.rustStructureOrAnyPsiModificationTracker: Any
             // literal. If a user change the literal, we can only be notified that the literal is changed.
             // So we have to invalidate the cached value on any PSI change
             containingFile.virtualFile is VirtualFileWindow ->
-                PsiModificationTracker.MODIFICATION_COUNT
+                PsiManager.getInstance(containingFile.project).modificationTracker
 
             containingFile.containingRsFileSkippingCodeFragments?.crate?.origin == PackageOrigin.WORKSPACE ->
-                project.rustStructureModificationTracker
+                containingFile.project.rustStructureModificationTracker
 
             else -> containingFile.project.rustPsiManager.rustStructureModificationTrackerInDependencies
         }

src/main/kotlin/org/rust/lang/core/psi/ext/RsElement.kt
@@ -8,6 +8,7 @@ package org.rust.lang.core.psi.ext
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.extapi.psi.StubBasedPsiElementBase
 import com.intellij.lang.ASTNode
+import com.intellij.openapi.util.UserDataHolderEx
 import com.intellij.psi.*
 import com.intellij.psi.search.SearchScope
 import com.intellij.psi.search.searches.ReferencesSearch
@@ -26,7 +27,7 @@ import org.rust.lang.core.macros.findNavigationTargetIfMacroExpansion
 import org.rust.lang.core.psi.*
 import org.rust.lang.core.resolve.*
 
-interface RsElement : PsiElement {
+interface RsElement : PsiElement, UserDataHolderEx {
     /**
      * Find parent module *in this file*. See [RsMod.super]
      */

src/main/kotlin/org/rust/lang/core/psi/ext/RsPossibleMacroCall.kt
@@ -8,14 +8,17 @@ package org.rust.lang.core.psi.ext
 import com.intellij.codeInsight.completion.CompletionUtil
 import com.intellij.extapi.psi.StubBasedPsiElementBase
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.util.CachedValue
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
 import com.intellij.psi.util.PsiModificationTracker
 import com.intellij.util.io.IOUtil
+import org.jetbrains.annotations.VisibleForTesting
 import org.rust.lang.core.crate.Crate
 import org.rust.lang.core.macros.*
 import org.rust.lang.core.macros.decl.MACRO_DOLLAR_CRATE_IDENTIFIER
@@ -425,7 +428,7 @@ val RsPossibleMacroCall.expansion: MacroExpansion?
 
 val RsPossibleMacroCall.expansionResult: RsResult<MacroExpansion, GetMacroExpansionError>
     get() {
-        return CachedValuesManager.getCachedValue(this) {
+        return CachedValuesManager.getCachedValue(this, RS_MACRO_CALL_EXPANSION_RESULT) {
             val originalOrSelf = CompletionUtil.getOriginalElement(this)?.takeIf {
                 // Use the original element only if macro bodies are equal. They
                 // will be different if completion invoked inside the macro body.
@@ -434,6 +437,10 @@ val RsPossibleMacroCall.expansionResult: RsResult<MacroExpansion, GetMacroExpans
             project.macroExpansionManager.getExpansionFor(originalOrSelf)
         }
     }
+
+@VisibleForTesting
+val RS_MACRO_CALL_EXPANSION_RESULT: Key<CachedValue<RsResult<MacroExpansion, GetMacroExpansionError>>> =
+    Key("org.rust.lang.core.psi.ext.RS_MACRO_CALL_EXPANSION_RESULT")
 
 fun RsPossibleMacroCall.expandMacrosRecursively(
     depthLimit: Int = getRecursionLimit(this),

src/test/kotlin/org/rustSlowTests/RsGcSoftlyReachableObjectsCacheTest.kt
@@ -6,12 +6,17 @@
 package org.rustSlowTests
 
 import com.intellij.util.ref.GCUtil
+import org.rust.ExpandMacros
 import org.rust.RsTestBase
 import org.rust.lang.core.crate.crateGraph
 import org.rust.lang.core.crate.impl.CrateGraphServiceImpl
+import org.rust.lang.core.psi.RsMacroCall
+import org.rust.lang.core.psi.RsPath
 import org.rust.lang.core.psi.RsStructItem
 import org.rust.lang.core.psi.RsTraitItem
+import org.rust.lang.core.psi.ext.RS_MACRO_CALL_EXPANSION_RESULT
 import org.rust.lang.core.psi.ext.descendantsOfType
+import org.rust.lang.core.psi.ext.expansionResult
 import org.rust.lang.core.psi.ext.withSubst
 import org.rust.lang.core.resolve.RsImplIndexAndTypeAliasCache
 import org.rust.lang.core.types.TraitRef
@@ -50,5 +55,27 @@ class RsGcSoftlyReachableObjectsCacheTest : RsTestBase() {
         check(canSelectImpl())
 
         check(impls.isNotEmpty()) // use `impls` in order to retain it in the memory until this moment
+    }
+
+    // Issue https://github.com/intellij-rust/intellij-rust/issues/9468
+    @ExpandMacros
+    fun `test only one in-memory macro expansion per macro call may exist in the memory at a time`() {
+        InlineFile("""
+            macro_rules! as_is { ($($ t:tt)*) => { $($ t)* } }
+            fn main() {
+                let a = 0;
+                as_is! {
+                    let b = a + 1;
+                }
+            }
+        """.trimIndent())
+
+        val macroCall = myFixture.file.descendantsOfType<RsMacroCall>().single()
+        val path = macroCall.expansionResult.ok()!!.file.descendantsOfType<RsPath>().single()
+        val cachedValue = macroCall.getUserData(RS_MACRO_CALL_EXPANSION_RESULT)!!
+
+        assertNotNull(path.reference!!.resolve())
+        GCUtil.tryGcSoftlyReachableObjects { !cachedValue.hasUpToDateValue() }
+        assertNotNull(path.reference!!.resolve())
     }
 }

commit e4670fd748f74feaf868506719c014dbf767dac8

COMP: fix completion in function-like macros
The bug has been introduced in 064f360895a510f1e381bebae9cd7142ead09d25
If you invoke completion in a macro call several times in different places without typing in the macro call,
it was possible that completion may stop working in the macro

src/main/kotlin/org/rust/lang/core/macros/MacroExpansionManager.kt
@@ -1019,7 +1019,7 @@ private data class CachedMemExpansionResult(
 
 private fun extractExpansionResult(
     cachedResultSoftReference: SoftReference<CachedMemExpansionResult>?,
-    modificationTrackers: Array<ModificationTracker>,
+    modificationTrackers: Array<Any>,
 ): CachedValueProvider.Result<RsResult<MacroExpansion, GetMacroExpansionError>>? {
     val cachedResult = cachedResultSoftReference?.get()
     if (cachedResult != null && cachedResult.modificationCount == modificationTrackers.modificationCount()) {
@@ -1039,17 +1039,24 @@ private fun memExpansionResult(
 }
 
 // Note: the cached result must be invalidated when `RsFile.cachedData` is invalidated
-private fun getModificationTrackersForMemExpansion(call: RsPossibleMacroCall): Array<ModificationTracker> {
+private fun getModificationTrackersForMemExpansion(call: RsPossibleMacroCall): Array<Any> {
     val structureModTracker = call.rustStructureOrAnyPsiModificationTracker
-    return if (call is RsMacroCall) {
-        arrayOf(structureModTracker, call.modificationTracker)
-    } else {
-        arrayOf(structureModTracker)
+    return when {
+        // Non-physical PSI does not have event system, but we can track the file changes
+        !call.isPhysical -> arrayOf(structureModTracker, call.containingFile)
+
+        call is RsMacroCall -> arrayOf(structureModTracker, call.modificationTracker)
+        else -> arrayOf(structureModTracker)
     }
 }
 
-private fun Array<ModificationTracker>.modificationCount(): Long =
-    sumOf { it.modificationCount }
+private fun Array<Any>.modificationCount(): Long = sumOf {
+    when (it) {
+        is ModificationTracker -> it.modificationCount
+        is PsiFile -> it.modificationStamp
+        else -> error("Unknown dependency: ${it.javaClass}")
+    }
+}
 
 private val RS_EXPANSION_MACRO_CALL = Key.create<RsPossibleMacroCall>("org.rust.lang.core.psi.RS_EXPANSION_MACRO_CALL")
 